bf70d08049ecf49f984c24b4ce8812c0f2cb61ad
shawn graham <sgraham5594@gmail.com>
2025-01-21 09:11:33 -0700

    week 3

--- app/page.js
+++ app/page.js
@@ -3,11 +3,11 @@ import Link from 'next/link';
 
 export default function Home(){
     return(
-        <body>
-<h1>CPRG 306: Web Development 2 - Assignments</h1>
+        <div>
+<h1><strong>CPRG 306: Web Development 2 - Assignments</strong></h1>
 <ul>
 <li> <Link href="/week-2">Week 2</Link></li>
-<li>Week 3</li>
+<li><Link href="/week-3">Week 3</Link></li>
 <li>Week 4</li>
 <li>Week 5</li>
 <li>Week 6</li>
@@ -16,6 +16,6 @@ export default function Home(){
 <li>Week 9</li>
 <li>Week 10</li>
 </ul>
-    </body>
+    </div>
 );
 }
