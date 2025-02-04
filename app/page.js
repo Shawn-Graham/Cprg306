d68be93411ec6908cfaca339e124fd46590d9b43
shawn graham <sgraham5594@gmail.com>
2025-02-03 22:13:06 -0700

    wekk-5

--- app/page.js
+++ app/page.js
@@ -9,7 +9,7 @@ export default function Home(){
 <li> <Link href="/week-2">Week 2</Link></li>
 <li><Link href="/week-3">Week 3</Link></li>
 <li><Link href="/week-4">Week 4</Link></li>
-<li>Week 5</li>
+<li><Link href="/week-5">Week 5</Link></li>
 <li>Week 6</li>
 <li>Week 7</li>
 <li>Week 8</li>
---
+++ app/week-5/page.js
@@ -0,0 +1,14 @@
+'use client';
+
+import React from 'react';
+import NewItem from './new-item'; // Adjust the path as necessary
+
+export default function Page() {
+  return (
+    <main className="flex justify-center ">
+    <div >
+    <NewItem />
+    </div>
+    </main>
+  );
+}
