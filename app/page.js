62c68601b608cd45f3baf2e0dc6d4fa6fc783700
shawn graham <sgraham5594@gmail.com>
2025-02-25 08:41:34 -0700

    home page css

--- app/page.js
+++ app/page.js
@@ -4,13 +4,14 @@ import Link from 'next/link';
 export default function Home(){
     return(
         <div>
-<h1><strong>CPRG 306: Web Development 2 - Assignments</strong></h1>
-<ul>
-<li> <Link href="/week-2">Week 2</Link></li>
-<li><Link href="/week-3">Week 3</Link></li>
-<li><Link href="/week-4">Week 4</Link></li>
-<li><Link href="/week-5">Week 5</Link></li>
-<li><Link href="/week-6">Week 6</Link></li>
+<h1 className="text-3xl font-bold text-center  m-2"><strong>CPRG 306: Web Development 2 - Assignments</strong></h1>
+<ul className="text-center">
+<li> <Link className="text-xl hover:text-blue-700" href="/week-2">Week 2</Link></li>
+<li><Link className="text-xl hover:text-blue-700" href="/week-3">Week 3</Link></li>
+<li><Link className="text-xl hover:text-blue-700"  href="/week-4">Week 4</Link></li>
+<li><Link className="text-xl hover:text-blue-700"  href="/week-4">Week 4</Link></li>
+<li><Link className="text-xl hover:text-blue-700" href="/week-5">Week 5</Link></li>
+<li><Link className="text-xl hover:text-blue-700"  href="/week-6">Week 6</Link></li>
 <li>Week 7</li>
 <li>Week 8</li>
 <li>Week 9</li>
